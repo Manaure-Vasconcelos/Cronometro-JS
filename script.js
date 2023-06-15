f4ddf4828bd3df0ced036b1b8690f3756e5f73b6
Manaure Vasconcelos <manaurevasc97@gmail.com>
2023-06-15 10:47:09 -0300

    JS ok

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ let milliseconds = 0;
 let isPaused = false;
 
 startBtn.addEventListener("click", comecar)
+pauseBtn.addEventListener("click", pausar)
+resumeBtn.addEventListener("click", continuar)
+resetBtn.addEventListener("click", resetar)
 
 function comecar() {
     interval = setInterval(() => {
@@ -29,9 +32,46 @@ function comecar() {
                 seconds = 0;
             }
 
-            minutesEl.textContent = minutes
-            secondsEl.textContent = seconds
-            millisecondsEl.textContent = milliseconds
+            minutesEl.innerHTML = formatTime(minutes)
+            secondsEl.innerHTML = formatTime(seconds)
+            millisecondsEl.innerHTML = formatMilliseconds(milliseconds)
         }
     }, 10)
+    startBtn.style.display = 'none'
+    pauseBtn.style.display = 'block'
 }
+
+function pausar() {
+    isPaused = true
+    pauseBtn.style.display = 'none'
+    resumeBtn.style.display = 'block'
+}
+
+function continuar() {
+    isPaused = false
+    resumeBtn.style.display = 'none'
+    pauseBtn.style.display = 'block'
+}
+
+function resetar() {
+    clearInterval(interval)
+    minutes = 0
+    seconds = 0
+    milliseconds = 0
+
+    minutesEl.innerHTML = "00"
+    secondsEl.innerHTML = "00"
+    millisecondsEl.innerHTML = "000"
+
+    startBtn.style.display = 'block'
+    pauseBtn.style.display = 'none'
+    resumeBtn.style.display = 'none'
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time
+}
+
+function formatMilliseconds(time){
+    return time < 100 ? `${time}`.padStart(3, "0") : time
+} 
